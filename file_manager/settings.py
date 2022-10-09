ccc547bbf1a57ba7a269c69342a78df470e6684d
Mohsen Hassani <m.hassani.de@gmail.com>
2022-10-09 20:46:37 +0400

    Add static root

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -107,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticroot'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
